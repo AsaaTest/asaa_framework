ae821875c11465e339ed72d2d440d9793be3ce56
AsaaTest <abelcoc06@gmail.com>
2023-07-23 00:51:20 -0500

    Login

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,16 @@ use Asaa\Http\Request;
 use Asaa\Http\Response;
 use Asaa\Routing\Route;
 
-Route::get('/', fn ($request) => Response::text(auth()->name));
+Route::get('/', function ($request) {
+    if(isGuest()){
+        return Response::text("Guest");
+    }
+    return Response::text(auth()->name);
+});
 Route::get('/form', fn ($request) => view("form"));
+
 Route::get('/register', fn ($request) => view("auth/register"));
+
 Route::post('/register', function (Request $request) {
     $data = $request->validate([
         'name' => 'required',
@@ -29,5 +36,30 @@ Route::post('/register', function (Request $request) {
 
     $user->login();
 
+    return redirect('/');
+});
+
+Route::get('/login', fn ($request) => view("auth/login"));
+
+Route::post('/login', function (Request $request) {
+    $data = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+
+    $user = User::firstWhere('email', $data["email"]);
+
+    if(is_null($user) || !app(Hasher::class)->verify($data["password"], $user->password)){
+        return back()->withErrors(["email" => ["email" => "Credentials do not match"]]);
+    }
+
+    $user->login();
+
+    return redirect("/");
+    
+});
+
+Route::get('/logout', function ($request) {
+    auth()->logout();
     return redirect('/');
 });
